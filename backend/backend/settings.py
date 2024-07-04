e04d04db7591b1fc2a77b8cc11c733d2f5a01179
peter115342 <pmuzslay8@gmail.com>
2024-07-05 00:09:14 +0200

    added DB connection and requirements.txt

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'datavisproject',
+        'USER': 'root',
+        'PASSWORD': 'password123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
